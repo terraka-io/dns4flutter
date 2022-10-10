903fab8beb18bc720de85230a06af3bb7e12c23d
Kale <wosikakale@foxmail.com>
2022-10-10 20:53:11 +0800

    完善

--- lib/dns_helper.dart
+++ lib/dns_helper.dart
@@ -1,17 +1,16 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:dns4flutter/dns_response.dart';
 import 'package:dns4flutter/src/buffer.dart';
 import 'package:dns4flutter/src/dns.dart';
 
-Future<void> main() async {
-  var resp = await DnsHelper.lookup('front.jetstream.site');
-  var host = resp?.host;
-  var web = resp?.web;
-  print(host);
-  print(web);
-}
+// use case
+// Future<void> main() async {
+//   var resp = await DnsHelper.lookupTxt('front.jetstream.site');
+//   var host = resp?.host;
+//   var web = resp?.web;
+//
+// }
 
 class DnsHelper {
   static const https = "https://";
@@ -23,25 +22,9 @@ class DnsHelper {
     "https://doh.360.cn/dns-query"
   ];
 
-  static DnsResponse _parseAddress(String rdata) {
-    //裁切
-    var list = rdata.split(":");
-    String web = "";
-    String host = "";
-    for (var element in list) {
-      if (element.contains("host")) {
-        host = https + element.split("=").last;
-      } else if (element.contains("web")) {
-        web = https + element.split("=").last;
-      }
-    }
-
-    return DnsResponse(host, web);
-  }
-
   static final _httpClient = HttpClient();
 
-  static Future<DnsResponse?> lookup(String host,
+  static Future<DnsResponse?> lookupTxt(String host,
       {List<String> dnsUrls = _defaultDnsUrls}) async {
     var requestBuffer = DNS.generateAMessage(host, type: DNS.QTYPE_TXT);
     var requestQuery = requestBuffer.toBase64().replaceAll('=', '');
@@ -49,9 +32,7 @@ class DnsHelper {
     for (var dnsUrl in dnsUrls) {
       try {
         var request = await _httpClient.getUrl(Uri.parse("$dnsUrl?dns=$requestQuery"));
-
         var response = await request.close();
-
         var responseBuffer = <int>[];
         await for (var part in response) {
           responseBuffer.addAll(part);
@@ -61,12 +42,30 @@ class DnsHelper {
         if (message.header.ancount > 0) {
           for (var record in message.answer) {
             var rdata = utf8.decode(record.rdata, allowMalformed: true);
-            return _parseAddress(rdata);
+            return _parseData(rdata);
           }
         }
       } catch (e) {
         print(e);
       }
+    }
+    return null;
+  }
+
+  static DnsResponse? _parseData(String data) {
+    //裁切
+    var list = data.split(":");
+    String web = "";
+    String host = "";
+    for (var element in list) {
+      if (element.contains("host")) {
+        host = https + element.split("=").last;
+      } else if (element.contains("web")) {
+        web = https + element.split("=").last;
+      }
+    }
+    if (web.isNotEmpty && host.isNotEmpty) {
+      return DnsResponse(host, web);
     }
     return null;
   }
